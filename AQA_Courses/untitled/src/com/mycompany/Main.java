4a8c62107e6fd1f5b73fa3985ab49fab6b68461b
xml12333 <xml12333@gmail.com>
2019-09-15 18:47:42 +0300

    commit from idea

--- AQA_Courses/untitled/src/com/mycompany/Main.java
+++ AQA_Courses/untitled/src/com/mycompany/Main.java
@@ -3,5 +3,5 @@ package com.mycompany;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("dfghj");    }
+        System.out.println("bevers attack");    }
 }
